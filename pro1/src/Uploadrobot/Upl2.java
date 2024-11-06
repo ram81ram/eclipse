7c5844bfa988bfad5e6a449c7bc01ed570f4b53f
ram <ram830377@gmail.com>
2024-11-06 11:25:26 +0530

    first commit

---
+++ pro1/src/Uploadrobot/Upl2.java
@@ -0,0 +1,10 @@
+package Uploadrobot;
+
+public class Upl2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ pro1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author RAM
+ *
+ */
+module pro1 {
+}
